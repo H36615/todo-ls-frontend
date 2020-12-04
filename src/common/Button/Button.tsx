a327971d96c6593d76855ffefbc3bc77aa01ba53
H36615 <h36615@protonmail.com>
2020-12-04 15:02:20 +0200

    Improve button

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 
 interface ButtonProps {
-	onClick: () => void,
+	/** E.g. button in form does not need to pass this if type is 'submit'. */
+	onClick: undefined | (() => void),
 	children: React.ReactNode,
+	disabled?: boolean,
+	type?: "button" | "submit" | "reset",
 }
 
-export default function Button({ children, onClick }: ButtonProps): JSX.Element {
+export default function Button(props: ButtonProps): JSX.Element {
 	return (
 		<div className="Button">
 			<button
-				className="items-center rounded-md
+				type={props.type || undefined}
+				className={`items-center rounded-md
+				text-gray-700 bg-white bg-opacity-80
 				px-3 py-1
-				text-gray-700
-				bg-white bg-opacity-80
-				hover:bg-gray-200 hover:bg-opacity-30
 				focus:outline-none
-				border"
+				border ${props.disabled ? "opacity-50" : "hover:bg-gray-200 hover:bg-opacity-30"}`}
 				style={{ minWidth: "10px" }}
-				onClick={onClick}>
-				<p className="bold">{children}</p>
+				onClick={props.onClick}>
+				<p className="bold">{props.children}</p>
 			</button>
 		</div>
 	);
