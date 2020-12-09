f79e3b868c19e5405de30c34da1af0b70a249828
H36615 <h36615@protonmail.com>
2020-12-09 22:43:05 +0200

    Create tabs for "sign in / sign up"

--- src/features/SignInOrUpModal/SignInOrUpModal.tsx
+++ src/features/SignInOrUpModal/SignInOrUpModal.tsx
@@ -11,13 +11,44 @@ interface SignInOrUpModalProps {
 export default function SignInOrUpModal(props: SignInOrUpModalProps): JSX.Element {
 
 	const [modalButtonsDisabled, setModalButtonsDisabled] = useState<boolean>(false);
+	/** Sign in enabled, otherwise sign up is enabled. */
+	const [signInEnabled, setSignInEnabled] = useState<boolean>(false);
 
 	return (
 		<div className="SignInOrUpModal">
 			<Modal modalOpen={props.modalOpen} header="Sign in / Sign up"
 				onCloseModal={props.closeModal} closeButtonDisabled={modalButtonsDisabled}>
 
-				<div>Sign in</div>
+				{/* Tab buttons */}
+				<div className="flex flex-row items-end">
+					<button
+						className={`items-center rounded-t-md
+						text-gray-700 bg-white bg-opacity-80
+						px-3 py-1 border flex-1
+						border-gray-300
+						focus:outline-none
+			${signInEnabled ?
+			"opacity-50 border-b-0" :
+			"hover:bg-gray-300 hover:bg-opacity-30 bg-gray-100 shadow-inline_bottom"}
+						`}
+						onClick={() => setSignInEnabled(true)}>
+						<p className="bold">Sign in</p>
+					</button>
+					<div className="h-1 w-1 border-b shadow-inline_bottom" />
+					<button
+						className={`items-center rounded-t-md
+						text-gray-700 bg-white bg-opacity-80
+						px-3 py-1 border flex-1
+						border-gray-300
+						focus:outline-none
+			${signInEnabled ?
+			"hover:bg-gray-200 hover:bg-opacity-30 bg-gray-100 shadow-inline_bottom" :
+			"opacity-50 border-b-0"}
+						`}
+						onClick={() => setSignInEnabled(false)}>
+						<p className="bold">Sign up</p>
+					</button>
+				</div>
 
 				<SignIn closeModal={props.closeModal}
 					setModalButtonsDisabled={
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
+		boxShadow: {
+			inline_bottom: "inset 0px -2px 4px 0 rgba(0, 0, 0, 0.06)"
+		}
 	},
 	variants: {
 		extend: {},
