19c98c4ad3c2016bb2e7636647abeedf92c590b4
H36615 <h36615@protonmail.com>
2020-12-20 15:22:32 +0200

    Add WIP icon button

---
+++ src/common/IconButton/IconButton.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable max-len */
+import React from "react";
+
+interface IconButtonProps {
+	/** E.g. button in form does not need to pass this if type is 'submit'. */
+	onClick: undefined | (() => void),
+	children: React.ReactNode,
+	disabled?: boolean,
+	type?: "button" | "submit" | "reset",
+}
+
+export default function IconButton(props: IconButtonProps): JSX.Element {
+	return (
+		<div className="Button">
+			<button
+				disabled={props.disabled}
+				type={props.type || undefined}
+				className={`items-center mx-auto flex-shrink-0 flex items-center
+				justify-center h-12 w-12 rounded-full bg-red-500 bg-opacity-10
+				focus:outline-none
+				sm:mx-0 sm:h-10 sm:w-10
+				${props.disabled ? "" : "hover:bg-opacity-30"}`}
+				style={{ minWidth: "10px" }}
+				onClick={props.onClick}>
+				{props.children}
+			</button>
+		</div>
+	);
+}
---
+++ src/icons/DeleteIcon.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable max-len */
+
+import React from "react";
+
+export default function DeleteIcon(): JSX.Element {
+	return (
+		<svg className="fill-current text-red-700" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z" /></svg>
+	);
+}
