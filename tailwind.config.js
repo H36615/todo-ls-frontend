074f19a53bea130d87a707aca049259bff53208d
H36615 <h36615@protonmail.com>
2020-12-28 00:48:33 +0200

    Use selected colors on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /* eslint-disable no-undef */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require("tailwindcss/colors");
+
 module.exports = {
 	purge: [
 		"./src/**/*.html",
@@ -9,6 +13,18 @@ module.exports = {
 		extend: {},
 		boxShadow: {
 			inline_bottom: "inset 0px -2px 4px 0 rgba(0, 0, 0, 0.06)"
+		},
+		colors: {
+			// Build your palette here
+			transparent: "transparent",
+			current: "currentColor",
+			black: colors.black,
+			white: colors.white,
+			gray: colors.trueGray,
+			green: colors.green,
+			red: colors.red,
+			blue: colors.blue,
+			yellow: colors.amber,
 		}
 	},
 	variants: {
