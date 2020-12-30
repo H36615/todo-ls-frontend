7fe93acebe03785d2180f7c8e39ae29b8a471b78
H36615 <h36615@protonmail.com>
2020-12-30 21:00:21 +0200

    Add updated backendUrl

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
 
 export const config = {
 	sourceCodeUrl: "https://github.com/H36615/todo-ls-frontend",
+	backendUrl: "https://todo-ls.h36615.dev",
 	publicTestUserEmail: "testuser@test.net",
 	publicTestUserPassword: "hunter21",
 };
