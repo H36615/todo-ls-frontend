c590d92bd4d9fc1b4ba48dc66d13e050cf8c3d00
H36615 <h36615@protonmail.com>
2020-12-31 16:27:42 +0200

    Change backend url & add error index page

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 
 export const config = {
 	sourceCodeUrl: "https://github.com/H36615/todo-ls-app",
-	backendUrl: "https://todo-ls.h36615.dev",
+	backendUrl: "https://api.h36615.dev",
 	publicTestUserEmail: "testuser@test.net",
 	publicTestUserPassword: "hunter21",
 };
