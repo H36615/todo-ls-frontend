972f0c687490ddad3eb51ef03a42235df86bdcaa
H36615 <h36615@protonmail.com>
2021-01-01 19:00:48 +0200

    Revert backend url change

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 
 export const config = {
 	sourceCodeUrl: "https://github.com/H36615/todo-ls-app",
-	backendUrl: "https://api.h36615.dev",
+	backendUrl: "https://todo-ls.h36615.dev",
 	publicTestUserEmail: "testuser@test.net",
 	publicTestUserPassword: "hunter21",
 };
